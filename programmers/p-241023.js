3adb90516c9fcdcb07929fc8209f87d4e5eb74cb
April-37 <150885908+April-37@users.noreply.github.com>
2024-10-23 22:20:30 +0900

    24.10.23 프로그래머스 코테 문제 풀이

---
+++ programmers/p-241023.js
@@ -0,0 +1,11 @@
+// lv.1 과일 장수
+
+function solution(k, m, score) {
+  var answer = 0;
+  score.sort(function (a,b) {
+      return b-a;
+  });
+  for(let i=m-1; i<score.length; i+=m)
+      answer+=score[i]*m;
+  return answer;
+}
